e042d783b3f3914247367101c7dffd61289bf45d
celestrial <eunseoemma@gmail.com>
2026-04-07 19:32:04 +0900

    first commit

---
+++ src/main/java/com/example/testspringbt/TestSpringBtApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringbt/TestSpringBtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
